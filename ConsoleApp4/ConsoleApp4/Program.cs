576c247e82d6ddc211d20c2cfc51cfd9b4d4c2da
Dilip Kumar <ethicalhackerdk@gmail.com>
2022-07-26 20:13:04 +0530

    cycle detection added

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -29,7 +29,7 @@ namespace ConsoleApp4
         // Function to add an edge into the graph
         public void AddEdge(int v, int w) { adj[v].Add(w); }
 
-        // A recursive function used by ProjetSort
+        // A recursive function used by ProjetSort()
         void ProjectSortUtil(int v, bool[] visited,
                                  Stack<int> stack)
         {
@@ -48,6 +48,52 @@ namespace ConsoleApp4
             // Push current vertex to
             // stack which stores result
             stack.Push(v);
+        }
+        // This function used by isCyclic() 
+        private bool isCyclicUtil(int i, bool[] visited,
+                                        bool[] recStack)
+        {
+
+            // Mark the current node as visited and 
+            // part of recursion stack 
+            if (recStack[i])
+                return true;
+
+            if (visited[i])
+                return false;
+
+            visited[i] = true;
+
+            recStack[i] = true;
+            List<int> children = adj[i];
+
+            foreach (int c in children)
+                if (isCyclicUtil(c, visited, recStack))
+                    return true;
+
+            recStack[i] = false;
+
+            return false;
+        }
+
+        // Returns true if the graph contains a 
+        // cycle, else false. 
+        private bool isCyclic()
+        {
+
+            // Mark all the vertices as not visited and 
+            // not part of recursion stack 
+            bool[] visited = new bool[V];
+            bool[] recStack = new bool[V];
+
+
+            // Call the recursive helper function to 
+            // detect cycle in different DFS trees 
+            for (int i = 0; i < V; i++)
+                if (isCyclicUtil(i, visited, recStack))
+                    return true;
+
+            return false;
         }
 
         // The function to do Topological Sort.
@@ -89,21 +135,21 @@ namespace ConsoleApp4
             }
             else
             {
-                    throw new FileNotFoundException("Check for input file is present at location " +filepath);
+                throw new FileNotFoundException("Check for input file present at location " + filepath);
             }
         }
 
         static void Main(string[] args)
         {
-            
+
             int counter = 0;
-           
+
             string[] proj_list;
             string[] lines;
             string[] p_pair;
             string[] dep_proj;
             string dependency;
-            
+
             // Refular experession for checking input Project list 
             Regex regex = new Regex(@"P+[0-9]");
 
@@ -121,23 +167,24 @@ namespace ConsoleApp4
             //read Project list from first line 
             try
             {
+
                 lines = Project.ReadInput(fileName);
                 string project = lines[0];
                 if (lines.Length == 0)
                 {
-                    throw new IndexOutOfRangeException("file not found");
+                    throw new ArgumentException("Input is not specified in the input file.");
                 }
 
-               
-                    if (!project.Contains(","))
-                    {
-                        throw new ArgumentException("Project list is not comma seperated");
-                    }
-                
+
+                if (!project.Contains(","))
+                {
+                    throw new ArgumentException("Project list is not comma seperated");
+                }
+
                 //convert the list into list of projects
                 proj_list = project.Split(',');
 
-                
+
                 foreach (string s in proj_list)
                 {
                     Match match = regex.Match(s);
@@ -153,12 +200,12 @@ namespace ConsoleApp4
                 //remove all unwanted spaces
                 dependency = dependency.Replace(" ", "");
 
-               
+
                 if (!dependency.Contains("),("))
                 {
                     throw new ArgumentException("Dependencies are not given in specified format ex. (P1,P2),(P2,P3)....");
                 }
-                
+
                 //replace seprator for the project dependenies
                 dependency = dependency.Replace("),(", "#");
 
@@ -169,39 +216,36 @@ namespace ConsoleApp4
                 //create project dependencies list via split 
                 dep_proj = dependency.Split('#');
                 Project process;
-                
-                try
-                {   
-                    //initialise the graph of project with number of projects.
-                    process = new Project(proj_list.Length);
-                    for (int i = 0; i < dep_proj.Length; i++)
-                    {
-                        p_pair = dep_proj[i].Replace("P", "").Split(',');
-                        
-                        // Add each depenencies into a graph
-                        process.AddEdge(Convert.ToInt32(p_pair[0]) - 1, Convert.ToInt32(p_pair[1]) - 1);
-                    }
 
-                    // Get Tropological Sort Project list
-                    proj_result = process.ProjectSort();
-                    Console.Write("Projects list is ");
-                    Console.WriteLine(lines[0]);
-                    Console.Write("Project dependencies");
-                    Console.WriteLine(lines[1]);
-                    Console.Write("Order of the Project Build is ");
-                    foreach (string s in proj_result)
-                    {
-                        counter++;
-                        Console.Write(s);
-                        if (counter < proj_result.Count())
-                        {
-                            Console.Write(">>");
-                        }
-                    }
-                }
-                catch (Exception e)
+
+                //initialise the graph of project with number of projects.
+                process = new Project(proj_list.Length);
+                for (int i = 0; i < dep_proj.Length; i++)
                 {
-                    Console.WriteLine("Check Input File");
+                    p_pair = dep_proj[i].Replace("P", "").Split(',');
+
+                    // Add each depenencies into a graph
+                    process.AddEdge(Convert.ToInt32(p_pair[0]) - 1, Convert.ToInt32(p_pair[1]) - 1);
+                }
+                if (process.isCyclic())
+                {
+                    throw new ArgumentException("There is cycle exisits in given dependencies");
+                }
+                // Get Tropological Sort Project list
+                proj_result = process.ProjectSort();
+                Console.Write("Projects list is ");
+                Console.WriteLine(lines[0]);
+                Console.Write("Project dependencies");
+                Console.WriteLine(lines[1]);
+                Console.Write("Order of the Project Build is ");
+                foreach (string s in proj_result)
+                {
+                    counter++;
+                    Console.Write(s);
+                    if (counter < proj_result.Count())
+                    {
+                        Console.Write(">>");
+                    }
                 }
             }
             catch (Exception e)
